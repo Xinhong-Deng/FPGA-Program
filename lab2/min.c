c075a222bbc19fdcc946d056a6d9b5df54052e2b
Zheyu Liu <46538726+liuzheyu1998@users.noreply.github.com>
2019-10-18 16:44:38 -0400

    Update min.c

--- lab2/min.c
+++ lab2/min.c
@@ -1,9 +1,10 @@
 extern	int	MIN_2(int	x,	int	y);
 int	 main()	{
-	int	a,	b,	c;
-	a =	1;
-	b =	2;	
-	c =	MIN_2(a,b);
-	if(c==a) return b;
-	else return a;
+	int	a[5] = {1, 2, 3, 4, 5};
+	int c = a[0];
+	int i = 0;
+	for (;i < 5;i++){
+		c =	MIN_2(a[i], c);
+	}
+	return c;
 }
